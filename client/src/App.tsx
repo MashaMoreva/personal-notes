9c2c0054ef1f0363178e0516dc7d50250d462788
Masha Moreva <mashamiskova@yandex.ru>
2024-02-22 14:43:39 +0300

    add-account

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { AuthForm } from "./components/AuthForm";
+import { Account } from "./components/Account/Account";
 
 function App() {
   return (
     <div className="app">
-      <AuthForm />
+      <Account />
     </div>
   );
 }
